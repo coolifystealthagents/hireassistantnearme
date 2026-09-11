a880fb79d62f724115ab04cc72ec611522d43857
SportsRecruiterSupport <sportsrecruitersupport@users.noreply.github.com>
2026-09-11 20:22:24 +0000

    Publish September 11 blog and research batch

--- app/data.ts
+++ app/data.ts
@@ -1063,8 +1063,9 @@ import { sep7BlogPosts } from './sep7-blog';
 import { sep8BlogPosts } from './sep8-blog';
 import { sep9BlogPosts } from './sep9-blog';
 import { sep10BlogPosts } from './sep10-blog';
+import { sep11BlogPosts } from './sep11-blog';
 
-export const blogPosts = [...baseBlogPosts, ...dailyBlogPosts, ...aug18BlogPosts, ...aug19BlogFinalPosts, ...aug20BlogPosts, ...aug21BlogPosts, ...aug23BlogPosts, ...aug31BlogPosts, ...sep1BlogPosts, ...sep2BlogPosts, ...sep3BlogPosts, ...sep4BlogPosts, ...sep7BlogPosts, ...sep8BlogPosts, ...sep9BlogPosts, ...sep10BlogPosts].sort((a, b) => b.published.localeCompare(a.published) || a.slug.localeCompare(b.slug));
+export const blogPosts = [...baseBlogPosts, ...dailyBlogPosts, ...aug18BlogPosts, ...aug19BlogFinalPosts, ...aug20BlogPosts, ...aug21BlogPosts, ...aug23BlogPosts, ...aug31BlogPosts, ...sep1BlogPosts, ...sep2BlogPosts, ...sep3BlogPosts, ...sep4BlogPosts, ...sep7BlogPosts, ...sep8BlogPosts, ...sep9BlogPosts, ...sep10BlogPosts, ...sep11BlogPosts].sort((a, b) => b.published.localeCompare(a.published) || a.slug.localeCompare(b.slug));
 
 export const routes = [
   '/',
